9052e4fc381f3d2f7f5858a819365f515778ee5e
Andrea Cognolato <andrecogno@hotmail.it>
2019-11-23 15:24:39 +0100

    Silence orange warnings

--- api.py
+++ api.py
@@ -2,8 +2,12 @@ from XPLAIN_class import XPLAIN_explainer
 
 
 def get_explanation(dataset, classifier):
-    explainer = XPLAIN_explainer(dataset, classifier, train_explain_set=True)
-    return explainer.explain_instance(explainer.instance_indices[0])
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        explainer = XPLAIN_explainer(dataset, classifier, train_explain_set=True)
+        return explainer.explain_instance(explainer.instance_indices[0])
 
 
 if __name__ == "__main__":
